3be3c86df13e28bbc62b494b2f7808acfc94ddb8
Alex <alex@mail.com>
2025-12-12 13:11:40 +0200

    minor fix admin fe

---
+++ backend/src/WebApi.Api/Migrations/20251212104527_RemoveUniqueIndexOnUserId.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Api.Migrations
+{
+    public partial class RemoveUniqueIndexOnUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP INDEX IF EXISTS IX_Banks_UserId;");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_UserId",
+                table: "Banks",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Banks_UserId",
+                table: "Banks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_UserId",
+                table: "Banks",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
